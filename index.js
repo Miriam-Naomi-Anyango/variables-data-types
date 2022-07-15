b3499b3c75c52711dfd80d6c65e63182ac8bf350
Miriam Anyango <minaoanya@gmail.com>
2022-07-15 07:09:56 +0300

    phase 1 variables and data types recap

---
+++ index.js
@@ -0,0 +1,17 @@
+let cat = "Rose"
+cat="ted"
+console.log(cat)
+
+
+const food = "cookie"
+console.log(food)
+
+// A variable is a container in which we can store values for later retrieval.
+
+/* Variable Names in Javascript*
+Start every variable name with a lowercase letter. Variable names starting with a number are not valid.
+Don't use spaces. If a variable name consists of multiple words, camelCaseYourVariableNames (see the camel humps?) instead of snake_casing_them (think of the underscore as a snake that swallowed the words).
+Don't use JavaScript reserved words (Links to an external site.) or future reserved words (Links to an external site.).
+
+Initializing Variables in JavaScript
+Initializing variables in JavaScript is really a two-step process: first, we declare the variable, then we assign a value to it.*/
